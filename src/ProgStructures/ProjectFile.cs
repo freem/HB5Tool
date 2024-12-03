f8a2ff4ca826d54f2e26ffdf8cbfa822216dcf59
freem <aj187@ajworld.net>
2024-12-03 11:58:33 -0600

    incomplete but adding anyways even if i remove it later

---
+++ src/ProgStructures/ProjectFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HB5Tool
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ProjectFile
+	{
+		/// <summary>
+		/// Path to League data associated with this project.
+		/// </summary>
+		public string LeagueFilePath;
+	}
+}
