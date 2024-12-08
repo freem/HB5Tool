5c12a9b28e6dacbf1fe68ba98447e9c2702484c0
freem <aj187@ajworld.net>
2024-12-08 00:27:08 -0600

    start thinking about an "EditorManager" class so people don't accidentally open the same player twice and wonder what's going on

---
+++ src/ProgStructures/EditorManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HB5Tool
+{
+	/// <summary>
+	/// Possible sources of various data used by editors.
+	/// </summary>
+	public enum EditorDataSources
+	{
+		/// <summary>
+		/// Unset and/or invalid
+		/// </summary>
+		Invalid = -1,
+
+		/// <summary>
+		/// Player export file (.BTR/.PIT)
+		/// </summary>
+		PlayerExport = 0,
+
+		/// <summary>
+		/// Team export file (.HB5)
+		/// </summary>
+		TeamExport = 1,
+
+		/// <summary>
+		/// League file (.LGD)
+		/// </summary>
+		League = 2
+	};
+
+	// for players:
+	// - data source
+	//   if .BTR/.PIT: filename
+	//   if .HB5: team player index
+	//   if .LGD: global player index
+
+	// for teams:
+	// - data source
+	//   if .HB5: filename
+	//   if .LGD: team index
+
+	/// <summary>
+	/// Manager for various dialogs.
+	/// </summary>
+	public class EditorManager
+	{
+		
+
+	}
+}
