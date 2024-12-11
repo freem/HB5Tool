81bad6989ac7d62c9330698315e22f29ae2a4c0d
freem <aj187@ajworld.net>
2024-12-11 17:59:03 -0600

    update EditorManager; still not implemented/finalized

--- src/ProgStructures/EditorManager.cs
+++ src/ProgStructures/EditorManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace HB5Tool
 {
@@ -31,6 +32,61 @@ namespace HB5Tool
 		League = 2
 	};
 
+	/// <summary>
+	/// Editor calling parameters.
+	/// Used as keys into the various "current editor" Dictionaries.
+	/// </summary>
+	public struct EditorParams
+	{
+		/// <summary>
+		/// Editor data source.
+		/// </summary>
+		public EditorDataSources Source;
+
+		/// <summary>
+		/// Filename if applicable, or String.Empty otherwise.
+		/// </summary>
+		public string Filename;
+
+		/// <summary>
+		/// Player/Team index if applicable, or -1 otherwise.
+		/// </summary>
+		public int index;
+
+		/// <summary>
+		/// Editor form associated with these params.
+		/// </summary>
+		public Form EditorForm;
+
+		public EditorParams(EditorDataSources _src, string _file, int _idx, Form _form)
+		{
+			Source = _src;
+			Filename = _file;
+			index = _idx;
+			EditorForm = _form;
+		}
+	}
+
+	/// <summary>
+	/// Batter representation for an editor.
+	/// </summary>
+	public struct EditorBatter
+	{
+		public BatterData Data;
+		// todo: does not handle anything but historical stats at the moment
+		public PlayerStats Stats;
+	}
+
+	/// <summary>
+	/// Pitcher representation for an editor.
+	/// </summary>
+	public struct EditorPitcher
+	{
+		public PitcherData Data;
+		// todo: does not handle anything but historical stats at the moment
+		public PlayerStats Stats;
+	}
+
 	// for players:
 	// - data source
 	//   if .BTR/.PIT: filename
@@ -45,9 +101,10 @@ namespace HB5Tool
 	/// <summary>
 	/// Manager for various dialogs.
 	/// </summary>
-	public class EditorManager
+	public static class EditorManager
 	{
-		
+		public static Dictionary<EditorParams, EditorBatter> Batters = new Dictionary<EditorParams, EditorBatter>();
 
+		public static Dictionary<EditorParams, EditorPitcher> Pitchers = new Dictionary<EditorParams, EditorPitcher>();
 	}
 }
