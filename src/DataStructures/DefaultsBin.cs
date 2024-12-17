d43074e1b3abc407e4fb1b67d690fb598eb3c188
freem <aj187@ajworld.net>
2024-12-17 14:09:33 -0600

    more work on DEFAULTS.BIN

--- src/DataStructures/DefaultsBin.cs
+++ src/DataStructures/DefaultsBin.cs
@@ -18,6 +18,16 @@ namespace HB5Tool
 		public static readonly int DEFAULTS_LEAGUE_PATH_MAX = 64;
 
 		/// <summary>
+		/// Maximum value for Team Skill values.
+		/// </summary>
+		public static readonly int TEAM_SKILL_MAX = 10;
+
+		/// <summary>
+		/// Maximum value for Game Speed value.
+		/// </summary>
+		public static readonly int GAME_SPEED_MAX = 16;
+
+		/// <summary>
 		/// Maximum volume value.
 		/// </summary>
 		public static readonly int VOLUME_MAX = 127;
@@ -54,7 +64,31 @@ namespace HB5Tool
 		/// offset 0, 64 characters max
 		public string CurLeagueFile;
 
-		// controller/joystick inputs are possibly somewhere in here
+		/// <summary>
+		/// Team 1 Input Set (3=Keyboard 1, 4=Keyboard 2)
+		/// </summary>
+		/// offset 0x40
+		public byte ControlSet1;
+
+		/// <summary>
+		/// Team 2 Input Set (3=Keyboard 1, 4=Keyboard 2)
+		/// </summary>
+		/// offset 0x41
+		public byte ControlSet2;
+
+		/// <summary>
+		/// Team 1 control (0=CPU, 1=Human)
+		/// </summary>
+		/// offset 0x42
+		public byte ControlTeam1;
+
+		/// <summary>
+		/// Team 2 control (0=CPU, 1=Human)
+		/// </summary>
+		/// offset 0x43
+		public byte ControlTeam2;
+
+		// todo: what's at 0x44-0x47??
 
 		/// <summary>
 		/// Exhibition Team 1 (Away)
@@ -68,7 +102,17 @@ namespace HB5Tool
 		/// offset 0x49
 		public byte ExhibitionTeam2;
 
-		// offset 0x4A (short?): large batter (1) and ump/catcher view (2), or 3 for both
+		// offset 0x4A (short): large batter (1) and ump/catcher view (2), or 3 for both
+		/// <summary>
+		/// View options. (1 = Large Batter; 2 = Catcher + Umpire; 3 = both)
+		/// </summary>
+		/// offset 0x4A
+		public short ViewOptions;
+
+		// Level of Play values:
+		// FEDCBA98 76543210
+		// ???????? ????????
+		// DH, Fielding Errors, Injuries, Pitch to Center, Pitching Crosshair, Pitcher Fatigue, Fielding Marker, Base Stealing, Player Ratings
 
 		/// <summary>
 		/// "Level of Play" settings for Team 1.
@@ -82,54 +126,89 @@ namespace HB5Tool
 		/// offset 0x4E
 		public short PlayLevelSettings_Team2;
 
+		// Manager Options values:
+		// FEDCBA98 76543210
+		// ???????? ????????
+		// Pitching, Batting, Fielding, Throwing, Base Running, Substitutions, Fielder Positions
+
 		/// <summary>
-		/// Manager settings for Team 1.
+		/// Manager Options for Team 1.
 		/// </summary>
 		/// offset 0x50
 		public short ManagerSettings_Team1;
 
 		/// <summary>
-		/// Manager settings for Team 2.
+		/// Manager Options for Team 2.
 		/// </summary>
 		/// offset 0x52
 		public short ManagerSettings_Team2;
 
 		/// <summary>
 		/// Team 1 Skill level.
+		/// Lower values = easier for team; Higher values = harder for team.
 		/// </summary>
 		/// offset 0x54
 		public short Skill_Team1;
 
 		/// <summary>
 		/// Team 2 Skill level.
+		/// Lower values = easier for team; Higher values = harder for team.
 		/// </summary>
 		/// offset 0x56
 		public short Skill_Team2;
 
-		// offset 0x58 (short?): game speed?
+		/// <summary>
+		/// Game speed.
+		/// </summary>
+		/// offset 0x58
+		public short GameSpeed;
 
-		// offset 0x5A (byte? short?): batting practice pitch types
-		// todo: map out values
+		/// <summary>
+		/// Batting Practice pitch types.
+		/// </summary>
+		/// offset 0x5A
+		/// todo: map out values
+		public short BattingPractice_Pitches;
 
-		// offset 0x5C (byte? short?): batting practice pitch zone (0=center, 1=, 2=, 3=random)
+		/// <summary>
+		/// Batting Practice pitch zone. (0=Center, 1=, 2=, 3=Random)
+		/// </summary>
+		/// offset 0x5C
+		/// todo: finish mapping out values
+		public short BattingPractice_PitchZone;
 
-		// offset 0x5E (short?): display mode? 0=pitcher, 1=batter, 2=pitcher/batter
+		/// <summary>
+		/// Display Mode (0=Pitcher view, 1=Batter view, 2=switch between Pitcher and Batter views)
+		/// </summary>
+		/// offset 0x5E
+		public short DisplayMode;
 
-		// offset 0x64: in-game sound options; all disabled = 0xC0, all enabled = 0xFF
-		// 0xC1 = sound effects
-		// 0xC3 = sound effects and crowd
-		// 0xC7 = sound effects, crowd, surround sound
-		// 0xCF = sound effects, crowd, surround sound, al michaels
-		// 0xDF = sound effects, crowd, surround sound, al michaels, pa echo
-		// 0xEF = sound effects, crowd, surround sound, al michaels, background music
-		// 76543210
-		// 11||||||
-		//   |||||+--- sound effects
-		//   ||||+---- crowd
-		//   |||+----- surround sound
-		//   ||+------ Al Michaels
-		//   |+------- PA Echo
-		//   +-------- background music
+		// todo: 0x60-0x63
+
+		/// <summary>
+		/// In-game sound options.
+		/// </summary>
+		/// offset 0x64
+		/// First/least significant byte holds all useful values.
+		/// All disabled = 0xC0; All enabled = 0xFF
+		/// 
+		/// 76543210
+		/// 11||||||
+		///   |||||+--- sound effects
+		///   ||||+---- crowd
+		///   |||+----- surround sound
+		///   ||+------ Al Michaels
+		///   |+------- PA Echo
+		///   +-------- background music
+		///
+		/// Example values for first byte:
+		/// 0xC1 = sound effects
+		/// 0xC3 = sound effects and crowd
+		/// 0xC7 = sound effects, crowd, surround sound
+		/// 0xCF = sound effects, crowd, surround sound, al michaels
+		/// 0xDF = sound effects, crowd, surround sound, al michaels, pa echo
+		/// 0xEF = sound effects, crowd, surround sound, al michaels, background music
+		public short SoundToggles;
 
 		/// <summary>
 		/// Home Run Derby Pitch Count. (0=3, 1=5, 2=10, 3=20)
@@ -137,17 +216,53 @@ namespace HB5Tool
 		/// offset 0x66
 		public short HomeRunDerbyPitchCount;
 
-		// offset 0x68 (short): if most significant bit is set, One Pitch Mode is enabled
+		/// <summary>
+		/// If the most significant bit is set, One Pitch Mode is enabled.
+		/// </summary>
+		/// offset 0x68
+		public short OnePitchMode;
 
-		// offset 0x6A (short?): in-game music volume (different from main music volume for some reason)
+		/// <summary>
+		/// In-game Music Volume. (Different from the main music volume for some reason.)
+		/// </summary>
+		/// offset 0x6A
+		public short GameMusicVolume;
 
-		// offset 0x72: something related to audio
+		// todo: 0x6C-0x71
 
-		// offset 0x7C (short): sound card port
+		/// <summary>
+		/// Perform initial setup on game boot if zero.
+		/// </summary>
+		/// offset 0x72
+		public short InitialSetup;
 
-		// offset 0x80 (short?): sound card IRQ
+		/// <summary>
+		/// Sound card type, as defined in DRVRLIST.BIN.
+		/// </summary>
+		/// offset 0x74
+		public int SoundCard_Type;
+		
+		// todo: offset 0x78 possibly also a sound card-related int
 
-		// offset 0x84 (short?): sound card DMA
+		/// <summary>
+		/// Sound card port.
+		/// </summary>
+		/// offset 0x7C
+		public int SoundCard_Port;
+
+		/// <summary>
+		/// Sound card IRQ channel.
+		/// </summary>
+		/// offset 0x80
+		public int SoundCard_IRQ;
+
+		/// <summary>
+		/// Sound card "high" DMA channel.
+		/// </summary>
+		/// offset 0x84
+		public int SoundCard_DMA;
+
+		// todo: 0x88-0x9B
 
 		/// <summary>
 		/// Main Sound volume.
@@ -160,6 +275,8 @@ namespace HB5Tool
 		/// </summary>
 		/// offset 0x9E
 		public short MusicVolume;
+
+		// controller/joystick inputs are possibly somewhere in here
 
 		#region Home Run Derby Scoreboard
 		/// <summary>
@@ -211,8 +328,10 @@ namespace HB5Tool
 		public short HomeRunDerbyScore_20;
 		#endregion
 
+		// todo: 0xFA-0xFB
+
 		/// <summary>
-		/// Team for Practice/Home Run Derby
+		/// Team for Practice/Home Run Derby.
 		/// </summary>
 		/// offset 0xFC
 		public byte PracticeTeam;
@@ -224,15 +343,32 @@ namespace HB5Tool
 		public DefaultsBin()
 		{
 			CurLeagueFile = string.Empty;
+			ControlSet1 = 0;
+			ControlSet2 = 0;
+			ControlTeam1 = 0;
+			ControlTeam2 = 0;
 			ExhibitionTeam1 = 0;
 			ExhibitionTeam2 = 0;
+			ViewOptions = 0;
 			PlayLevelSettings_Team1 = 0;
 			PlayLevelSettings_Team2 = 0;
 			ManagerSettings_Team1 = 0;
 			ManagerSettings_Team2 = 0;
 			Skill_Team1 = 0;
 			Skill_Team2 = 0;
+			GameSpeed = 0;
+			BattingPractice_Pitches = 0;
+			BattingPractice_PitchZone = 0;
+			DisplayMode = 0;
+			SoundToggles = 0;
 			HomeRunDerbyPitchCount = 0;
+			OnePitchMode = 0;
+			GameMusicVolume = 64;
+			InitialSetup = 0;
+			SoundCard_Type = 0;
+			SoundCard_Port = 0x220;
+			SoundCard_IRQ = 7;
+			SoundCard_DMA = 5;
 			SoundVolume = 64;
 			MusicVolume = 64;
 			HomeRunDerbyName_3 = string.Empty;
@@ -246,12 +382,20 @@ namespace HB5Tool
 			PracticeTeam = 0;
 		}
 
+		/// <summary>
+		/// Constructor using a BinaryReader.
+		/// </summary>
+		/// <param name="br">BinaryReader instance to use.</param>
 		public DefaultsBin(BinaryReader br)
 		{
 			ReadData(br);
 		}
 		#endregion
 
+		/// <summary>
+		/// Read DEFAULTS.BIN data using a BinaryReader.
+		/// </summary>
+		/// <param name="br">BinaryReader instance to use.</param>
 		public void ReadData(BinaryReader br)
 		{
 			// loaded league string
@@ -271,27 +415,57 @@ namespace HB5Tool
 				}
 			}
 
-			// todo: everything from 0x40-0x9B
+			ControlSet1 = br.ReadByte();
+			ControlSet2 = br.ReadByte();
+			ControlTeam1 = br.ReadByte();
+			ControlTeam2 = br.ReadByte();
+
+			// todo: offsets 0x44-0x47
 
 			br.BaseStream.Seek(0x48, SeekOrigin.Begin);
 			ExhibitionTeam1 = br.ReadByte();
 			ExhibitionTeam2 = br.ReadByte();
-
-			br.BaseStream.Seek(0x4C, SeekOrigin.Begin);
+			ViewOptions = BitConverter.ToInt16(br.ReadBytes(2), 0);
 			PlayLevelSettings_Team1 = BitConverter.ToInt16(br.ReadBytes(2), 0);
 			PlayLevelSettings_Team2 = BitConverter.ToInt16(br.ReadBytes(2), 0);
 			ManagerSettings_Team1 = BitConverter.ToInt16(br.ReadBytes(2), 0);
 			ManagerSettings_Team2 = BitConverter.ToInt16(br.ReadBytes(2), 0);
 			Skill_Team1 = BitConverter.ToInt16(br.ReadBytes(2), 0);
 			Skill_Team2 = BitConverter.ToInt16(br.ReadBytes(2), 0);
+			GameSpeed = BitConverter.ToInt16(br.ReadBytes(2), 0);
+			BattingPractice_Pitches = BitConverter.ToInt16(br.ReadBytes(2), 0);
+			BattingPractice_PitchZone = BitConverter.ToInt16(br.ReadBytes(2), 0);
+			DisplayMode = BitConverter.ToInt16(br.ReadBytes(2), 0);
 
-			br.BaseStream.Seek(0x66, SeekOrigin.Begin);
+			// todo: 0x60-0x63
+
+			br.BaseStream.Seek(0x64, SeekOrigin.Begin);
+			SoundToggles = BitConverter.ToInt16(br.ReadBytes(2), 0);
 			HomeRunDerbyPitchCount = BitConverter.ToInt16(br.ReadBytes(2), 0);
+			OnePitchMode = BitConverter.ToInt16(br.ReadBytes(2), 0);
+			GameMusicVolume = BitConverter.ToInt16(br.ReadBytes(2), 0);
+
+			// todo: 0x6C-0x71
+
+			br.BaseStream.Seek(0x72, SeekOrigin.Begin);
+			InitialSetup = BitConverter.ToInt16(br.ReadBytes(2), 0);
+			SoundCard_Type = BitConverter.ToInt32(br.ReadBytes(4), 0);
+
+			// todo: 0x78 is possibly another sound card related int
+
+			br.BaseStream.Seek(0x7C, SeekOrigin.Begin);
+			SoundCard_Port = BitConverter.ToInt32(br.ReadBytes(4), 0);
+			SoundCard_IRQ = BitConverter.ToInt32(br.ReadBytes(4), 0);
+			SoundCard_DMA = BitConverter.ToInt32(br.ReadBytes(4), 0);
+
+			// todo: 0x88-0x9B
 
 			// skip to next area
 			br.BaseStream.Seek(0x9C,SeekOrigin.Begin);
 			SoundVolume = BitConverter.ToInt16(br.ReadBytes(2), 0);
 			MusicVolume = BitConverter.ToInt16(br.ReadBytes(2), 0);
+
+			// todo: 0xA0-0xB1
 
 			// skip to home run area
 			br.BaseStream.Seek(0xB2,SeekOrigin.Begin);
@@ -371,7 +545,7 @@ namespace HB5Tool
 			// score 20
 			HomeRunDerbyScore_20 = BitConverter.ToInt16(br.ReadBytes(2), 0);
 
-			// todo PracticeTeam at 0xFC
+			// PracticeTeam at 0xFC
 			br.BaseStream.Seek(0xFC, SeekOrigin.Begin);
 			PracticeTeam = br.ReadByte();
 		}
--- src/Editors/DefaultsBinEditor.cs
+++ src/Editors/DefaultsBinEditor.cs
@@ -12,6 +12,12 @@ namespace HB5Tool
 {
 	public partial class DefaultsBinEditor : Form
 	{
+		#region Value to String Arrays
+		public readonly string[] HomeRunDerbyPitchCounts = { "3", "5", "10", "20" };
+
+		public readonly string[] DisplayModes = { "Pitcher", "Batter", "Pitcher/Batter" };
+		#endregion
+
 		/// <summary>
 		/// Provided solely so the main form can hide the Window menu if the last form is closed.
 		/// </summary>
@@ -47,8 +53,19 @@ namespace HB5Tool
 			sb.AppendLine(string.Format("Current League File: {0}", CurDefaults.CurLeagueFile));
 			sb.AppendLine();
 
+			sb.AppendLine(string.Format("Team 1 Control Set: 0x{0:X2}", CurDefaults.ControlSet1));
+			sb.AppendLine(string.Format("Team 2 Control Set: 0x{0:X2}", CurDefaults.ControlSet2));
+			sb.AppendLine(string.Format("Team 1 Control: 0x{0:X2}", CurDefaults.ControlTeam1));
+			sb.AppendLine(string.Format("Team 2 Control: 0x{0:X2}", CurDefaults.ControlTeam2));
+
 			sb.AppendLine(string.Format("Exhibition Team 1: 0x{0:X2}", CurDefaults.ExhibitionTeam1));
 			sb.AppendLine(string.Format("Exhibition Team 2: 0x{0:X2}", CurDefaults.ExhibitionTeam2));
+			sb.AppendLine();
+
+			sb.AppendLine(string.Format("View Options: 0x{0:X4}", CurDefaults.ViewOptions));
+			sb.AppendLine(string.Format("Display Mode: 0x{0:X4} ({1})", CurDefaults.DisplayMode, DisplayModes[CurDefaults.DisplayMode]));
+			sb.AppendLine();
+
 			sb.AppendLine(string.Format("Team 1 Play Level Settings: 0x{0:X4}", CurDefaults.PlayLevelSettings_Team1));
 			sb.AppendLine(string.Format("Team 2 Play Level Settings: 0x{0:X4}", CurDefaults.PlayLevelSettings_Team2));
 			sb.AppendLine(string.Format("Team 1 Manager Settings: 0x{0:X4}", CurDefaults.ManagerSettings_Team1));
@@ -57,12 +74,28 @@ namespace HB5Tool
 			sb.AppendLine(string.Format("Team 2 Skill Level: 0x{0:X4}", CurDefaults.Skill_Team2));
 			sb.AppendLine();
 
-			sb.AppendLine(string.Format("Practice/HR Derby Team: 0x{0:X2}", CurDefaults.PracticeTeam));
-			sb.AppendLine(string.Format("HR Derby Pitch Count: 0x{0:X2}", CurDefaults.HomeRunDerbyPitchCount));
+			sb.AppendLine(string.Format("Game Speed: 0x{0:X4}", CurDefaults.GameSpeed));
 			sb.AppendLine();
 
-			sb.AppendLine(string.Format("Sound Volume: {0}", CurDefaults.SoundVolume));
-			sb.AppendLine(string.Format("Music Volume: {0}", CurDefaults.MusicVolume));
+			sb.AppendLine(string.Format("Batting Practice Pitches: 0x{0:X4}", CurDefaults.BattingPractice_Pitches));
+			sb.AppendLine(string.Format("Batting Practice Pitch Zone: 0x{0:X4}", CurDefaults.BattingPractice_PitchZone));
+			sb.AppendLine();
+
+			sb.AppendLine(string.Format("Practice/HR Derby Team: 0x{0:X2}", CurDefaults.PracticeTeam));
+			sb.AppendLine(string.Format("HR Derby Pitch Count: 0x{0:X2} ({1} pitches)", CurDefaults.HomeRunDerbyPitchCount, HomeRunDerbyPitchCounts[CurDefaults.HomeRunDerbyPitchCount]));
+			sb.AppendLine();
+
+			sb.AppendLine(string.Format("Sound Volume: {0} ({1}%)", CurDefaults.SoundVolume, ((float)CurDefaults.SoundVolume/DefaultsBin.VOLUME_MAX)*100));
+			sb.AppendLine(string.Format("Music Volume: {0} ({1}%)", CurDefaults.MusicVolume, ((float)CurDefaults.MusicVolume/DefaultsBin.VOLUME_MAX)*100));
+			sb.AppendLine(string.Format("Game Music Volume: {0} ({1}%)", CurDefaults.GameMusicVolume, ((float)CurDefaults.GameMusicVolume / DefaultsBin.VOLUME_MAX)*100));
+			sb.AppendLine(string.Format("Sound Toggles: 0x{0:X2}", CurDefaults.SoundToggles));
+			sb.AppendLine();
+
+			sb.AppendLine("Sound Card Settings");
+			sb.AppendLine(string.Format("Type: 0x{0:X4}", CurDefaults.SoundCard_Type));
+			sb.AppendLine(string.Format("Port: 0x{0:X2}", CurDefaults.SoundCard_Port));
+			sb.AppendLine(string.Format("IRQ:  {0}", CurDefaults.SoundCard_IRQ));
+			sb.AppendLine(string.Format("DMA:  {0}", CurDefaults.SoundCard_DMA));
 			sb.AppendLine();
 
 			sb.AppendLine("Home Run Derby High Scores");
